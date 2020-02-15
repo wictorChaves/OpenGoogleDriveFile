0ed1aadcec90e578bf445c5c825bba26ba80713c
wictor <wictor@hotmail.com.br>
2020-02-15 14:00:01 -0300

    Arquivo abrindo.

--- OpenGoogleDriveFile/Program.cs
+++ OpenGoogleDriveFile/Program.cs
@@ -1,14 +1,47 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Threading;
 
 namespace OpenGoogleDriveFile
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
+            if (args.Length == 0) Environment.Exit(0);
+            Thread thread = new Thread(Program.Start);
+            thread.Start(args[0]);
+        }
+
+        public static void Start(object fileUrl)
+        {
+            string browser = ConfigurationManager.AppSettings["browser"];
+            using (StreamReader r = new StreamReader(fileUrl.ToString()))
+            {
+                string json = r.ReadToEnd();
+                Model model = JsonConvert.DeserializeObject<Model>(json);
+                _openBrowser(browser, model.url);
+            }
+        }
+
+        private static void _openBrowser(string browser, string file)
+        {
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = browser;
+            pProcess.StartInfo.Arguments = file;
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            pProcess.StartInfo.CreateNoWindow = false;
+            pProcess.Start();
+            string output = pProcess.StandardOutput.ReadToEnd();
+            pProcess.WaitForExit();
         }
     }
 }
---
+++ OpenGoogleDriveFile/model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenGoogleDriveFile
+{
+    class Model
+    {
+        public string url { get; set; }
+        public string doc_id { get; set; }
+        public string email { get; set; }
+    }
+}
